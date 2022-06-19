3cebbbebfdb1022c85c895338a5cf44c644b6fdd
Kirasumairu <1479263@gmail.com>
2022-06-20 00:29:21 +0200

    Error handling

--- web/main.py
+++ web/main.py
@@ -23,3 +23,11 @@ def index():
 @app.route('/user/<name>')
 def name(name):
   return render_template('user.html', user_name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+  return render_template('500.html'), 500
