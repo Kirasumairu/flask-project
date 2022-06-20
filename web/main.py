22369fef9bdaea647dd39a230f205ca65c286292
Kirasumairu <1479263@gmail.com>
2022-06-20 23:22:00 +0200

    Individual Blog Post

--- web/main.py
+++ web/main.py
@@ -121,6 +121,11 @@ def posts():
   posts = Posts.query.order_by(Posts.date_posted)
   return render_template('posts.html', posts=posts)
 
+@app.route('/posts/<int:id>')
+def post(id):
+  post = Posts.query.get_or_404(id)
+  return render_template('post.html', post=post)
+
 @app.route('/favorite_pizza')
 def get_favorite_pizza():
   favorite_piza = {
