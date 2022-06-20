c1641e71a46d20c46180b88a3948ee39703e203d
Kirasumairu <1479263@gmail.com>
2022-06-20 17:32:15 +0200

    Changed to MySQL

--- web/main.py
+++ web/main.py
@@ -9,7 +9,8 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@mysql/users'
 app.config['SECRET_KEY'] = 'supersecretkey'
 
 db = SQLAlchemy(app)
