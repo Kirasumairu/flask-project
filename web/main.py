6ac59368e44ca8b1997585e382f4b9892def0d74
Kirasumairu <1479263@gmail.com>
2022-06-20 14:21:36 +0200

    SQL with SQLAlchemy

--- web/main.py
+++ web/main.py
@@ -2,14 +2,38 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # filters: safe capitalize upper lower title trim striptags
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = 'supersecretkey'
 
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(200), nullable=False)
+  email = db.Column(db.String(120), nullable=False, unique=True)
+  date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Name %r>' % self.name
+
+# from the container
+# from main import db
+# db.create_all()
+# 
 class NameForm(FlaskForm):
-  name = StringField('What\'s Your Name', validators=[DataRequired()])
+  name = StringField('Name', validators=[DataRequired()])
+  submit = SubmitField()
+
+class UserForm(FlaskForm):
+  name = StringField('Name', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired()])
   submit = SubmitField()
 
 @app.route('/')
@@ -22,6 +46,28 @@ def index():
     first_name=first_name,
     code=code,
     favorite_pizza=favorite_pizza
+  )
+
+@app.route('/users/add', methods=['GET', 'POST'])
+def add_user():
+  name = None
+  form = UserForm()
+  if form.validate_on_submit():
+    user = Users.query.filter_by(email=form.email.data).first()
+    if user is None:
+      user = Users(name=form.name.data, email=form.email.data)
+      db.session.add(user)
+      db.session.commit()
+    name = form.name.data
+    form.name.data = ''
+    form.email.data = ''
+    flash('User Added Successfully!')
+  our_users = Users.query.order_by(Users.date_added)
+  return render_template(
+    'add_user.html',
+    form=form,
+    name=name,
+    our_users=our_users
   )
 
 @app.route('/user/<name>')
