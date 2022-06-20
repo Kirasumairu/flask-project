199d2efa84f1fc1ab7cf438d4c6cdd6c0f6f4b31
Kirasumairu <1479263@gmail.com>
2022-06-20 18:37:24 +0200

    Delete db records

--- web/main.py
+++ web/main.py
@@ -81,6 +81,27 @@ def update(id):
     user_to_update=user_to_update
   )
 
+@app.route('/delete/<int:id>')
+def delete(id):
+  user_to_delete = Users.query.get_or_404(id)
+  name = None
+  form = UserForm()
+  try:
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    flash('User Deleted Successfully!')
+  except:
+    flash('Error! Looks like there was a problem... Try again')
+
+  our_users = Users.query.order_by(Users.date_added)
+  return render_template(
+    'add_user.html',
+    form=form,
+    name=name,
+    our_users=our_users
+  )
+
+
 @app.route('/users/add', methods=['GET', 'POST'])
 def add_user():
   name = None
